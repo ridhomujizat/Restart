185482d0d58e2cf6b4735c603de3695ac66b13b7
ridhotitipku <107243155+ridhotitipku@users.noreply.github.com>
2022-10-15 17:37:01 +0700

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by ridho mujizat on 15/10/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
