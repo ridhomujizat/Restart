f0d4409fc2f32395cf8d01e52eb8c36e87d46231
ridhotitipku <107243155+ridhotitipku@users.noreply.github.com>
2022-10-19 22:55:35 +0700

    Gesture to home

---
+++ Restart/Screens/OnboardingView.swift
@@ -0,0 +1,126 @@
+//
+//  OnboardingView.swift
+//  Restart
+//
+//  Created by ridho mujizat on 15/10/22.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80;
+    @State private var buttonOffets: CGFloat = 0;
+    
+    var body: some View {
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            
+            VStack(spacing: 20) {
+                VStack(spacing: 0){
+                    Spacer()
+                    
+                    VStack(spacing: 0){
+                        Text("Share.")
+                            .font(.system(size: 60))
+                            .fontWeight(.heavy)
+                            .foregroundColor(.white)
+                        
+                        Text("""
+                        It's not how much we give but
+                        how much love we put inito giving
+                        """)
+                        .font(.title3)
+                        .fontWeight(.light)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 10)
+                    }
+                    
+                    ZStack {
+                        CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+                        Image("character-1")
+                            .resizable()
+                            .scaledToFit()
+                    }
+                    
+                    Spacer()
+                    
+                    ZStack {
+                        Capsule()
+                            .fill(Color.white.opacity(0.2))
+                        Capsule()
+                            .fill(Color.white.opacity(0.2))
+                            .padding(8)
+                        
+                        Text("Get Started")
+                            .font(.system(.title3, design: .rounded))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .offset(x: 20)
+                        
+                        HStack{
+                            Capsule()
+                                .fill(Color("ColorRed"))
+                                .frame(width: buttonOffets+80)
+                            
+                            Spacer()
+                        }
+                        
+                        HStack {
+                            ZStack{
+                                Circle()
+                                    .fill(Color("ColorRed"))
+                                Circle()
+                                    .fill(.black.opacity(0.15))
+                                    .padding(8)
+                                Image(systemName: "chevron.right.2")
+                                    .font(.system(size: 24, weight: .bold))
+                                
+                                
+                            }
+                            .foregroundColor(.white)
+                            .frame(width: 80, height: 80, alignment: .center)
+                            .offset(x: buttonOffets)
+                            
+                            .gesture(
+                                DragGesture()
+                                    .onChanged {
+                                        gesture in
+                                        if gesture.translation.width > 0 && buttonOffets <= buttonWidth  - 80 {
+                                            buttonOffets = gesture.translation.width
+                                        }
+                                    }.onEnded{ _ in
+                                        if buttonOffets > buttonWidth / 2 {
+                                            buttonOffets = buttonWidth - 80
+                                            isOnboardingViewActive = false
+                                        }else {
+                                            buttonOffets = 0
+                                        }
+
+                                    }
+                            )
+                            
+                            Spacer()
+                        }
+                        
+                        
+                    }
+                    .frame(width:buttonWidth,height: 80, alignment: .center)
+                    .padding()
+                    
+                    
+                }
+            }
+        }
+    }
+
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
